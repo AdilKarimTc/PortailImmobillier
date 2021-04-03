b940a146fd7005ba08852e5ceb540aa407b8d24c
ADIL KARIM <adilkarimscience@gmail.com>
2021-04-03 19:41:54 +0200

    added login ui

---
+++ src/PortailImmobillier.Web/Controllers/ComptesController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortailImmobillier.Web.Controllers
+{
+    public class ComptesController : Controller
+    {
+        public IActionResult Login(){
+            return View();
+        }
+
+        public IActionResult Register(){
+            return View();
+        }
+
+    }
+}
