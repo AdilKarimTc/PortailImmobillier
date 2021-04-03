4fe287c6a46234b45c383a94e00ae28fc3a1134f
ADIL KARIM <adilkarimscience@gmail.com>
2021-04-03 14:42:30 +0200

    can add and run migrations

--- src/PortailImmobillier.Data/Entities/Annonce.cs
+++ src/PortailImmobillier.Data/Entities/Annonce.cs
@@ -1,7 +1,8 @@
 namespace PortailImmobillier.Data.Entities{
     public class Annonce : BaseEntity
     {
-
+        public string Title{ get; set;}
+        
 
 
     }
---
+++ src/PortailImmobillier.Data/Migrations/ApplicationDb/20210403122250_Initial Application Migration.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PortailImmobillier.Data.Migrations.ApplicationDb
+{
+    public partial class InitialApplicationMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Annonces",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsSupprime = table.Column<bool>(type: "bit", nullable: false),
+                    CreeLe = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifieLe = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SupprimeLe = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Annonces", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Ville = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsSupprime = table.Column<bool>(type: "bit", nullable: false),
+                    CreeLe = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifieLe = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SupprimeLe = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Annonces");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
