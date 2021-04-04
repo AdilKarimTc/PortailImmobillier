33e0c56b8773eb8f825a0b3362392a6a70f96f37
ADIL KARIM <adilkarimscience@gmail.com>
2021-04-04 18:06:57 +0200

    added Html for Add Annonce

--- src/PortailImmobillier.Web/Controllers/AnnoncesController.cs
+++ src/PortailImmobillier.Web/Controllers/AnnoncesController.cs
@@ -1,15 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using PortailImmobillier.Web.Models;
 
 namespace PortailImmobillier.Web.Controllers{
     
     public class AnnoncesController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Add(AnnoncesModel model)
+        {
+            throw new NotImplementedException();
+        }
 
     }
+    
+
 
 
 }
---
+++ src/PortailImmobillier.Web/Models/AnnoncesModel.cs
@@ -0,0 +1,15 @@
+namespace PortailImmobillier.Web.Models
+{
+    public class AnnoncesModel
+    {
+        public string Titre{ get; set; }
+        public string ImageUrl{ get; set; }
+        public double Prix{ get; set; }
+        public string Description{ get; set;}
+        public int Chambres{ get; set; }
+        public int Toilettes{ get; set;}
+        public string Adresse{ get; set; }
+
+        public string ContactPhoneNumber{ get; set; }
+    }
+}
