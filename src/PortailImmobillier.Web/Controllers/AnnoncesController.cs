f429c0c7dd1ef6ce8b235e3979ac45af6be647cb
ADIL KARIM <adilkarimscience@gmail.com>
2021-04-04 22:22:39 +0200

    Added Authorization

--- src/PortailImmobillier.Web/Controllers/AnnoncesController.cs
+++ src/PortailImmobillier.Web/Controllers/AnnoncesController.cs
@@ -3,34 +3,39 @@ using System;
 using PortailImmobillier.Web.Models;
 using PortailImmobillier.Web.Interfaces;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using PortailImmobillier.Data.Entities;
+using Microsoft.AspNetCore.Authorization; 
 
 
 namespace PortailImmobillier.Web.Controllers{
     
+    [Authorize]
     public class AnnoncesController : Controller
     {
         private readonly IAnnoncesService _annonceService;
+        
         public AnnoncesController(IAnnoncesService annonceService)
         {
             _annonceService = annonceService;
         }
 
 
+        [AllowAnonymous]
         [HttpGet]
         public IActionResult Index()
         {
             var annonces = _annonceService.GetAllAnnonces();
             return View(annonces);
         }
+
+        [Authorize]
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
 
-        
+        [Authorize]
+        [HttpPost]
         public async Task<IActionResult> Add(AnnoncesModel model)
         {
             try 
